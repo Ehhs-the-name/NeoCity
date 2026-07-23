169edf0dfeb9badad776b6340de755aa930b2d1d
Ehh-the-name <keffendi2010@gmail.com>
2026-07-22 21:04:18 -0400

    avtr img formating

--- mp-script.js
+++ mp-script.js
@@ -5,10 +5,10 @@ function changeSVG(event){
     let height = document.getElementById("fullBody").offsetHeight;
 
     if (y < (height/2)) {
-        // up  --  https://i.postimg.cc/CxCH4SZn/Webbed-Site2.png
-        img.src = "https://i.postimg.cc/CxCH4SZn/Webbed-Site2.png";
+        // up
+        img.src = "https://i.postimg.cc/VLbngMGg/Webbed-Site2.png";
     } else {
-        // down --   https://i.postimg.cc/Bn5cC48L/Webbed-Site.png
-        img.src = "https://i.postimg.cc/Bn5cC48L/Webbed-Site.png";
+        // down
+        img.src = "https://i.postimg.cc/440ppw5r/Webbed-Site.png";
     }   
 }
