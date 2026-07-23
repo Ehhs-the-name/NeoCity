2834132c9c05f4cd1cd483c237e27fa37766c714
Ehh-the-name <keffendi2010@gmail.com>
2026-07-22 20:51:13 -0400

    avatar js complete

--- mp-script.js
+++ mp-script.js
@@ -1,14 +1,14 @@
-function changeSVG(e){
-    let svgImg = document.getElementById('svg');
-    let y = event.screenY;
-    let height = window.outerHeight;
 
-    if (y <= (height/2)) {
-        // up  https://i.postimg.cc/CxCH4SZn/Webbed-Site2.png
+function changeSVG(event){
+    let img = document.getElementById("fakeSVG");
+    let y = event.clientY;
+    let height = document.getElementById("fullBody").offsetHeight;
+
+    if (y < (height/2)) {
+        // up  --  https://i.postimg.cc/CxCH4SZn/Webbed-Site2.png
         img.src = "https://i.postimg.cc/CxCH4SZn/Webbed-Site2.png";
     } else {
-        // down  https://i.postimg.cc/Bn5cC48L/Webbed-Site.png
+        // down --   https://i.postimg.cc/Bn5cC48L/Webbed-Site.png
         img.src = "https://i.postimg.cc/Bn5cC48L/Webbed-Site.png";
     }   
 }
-
