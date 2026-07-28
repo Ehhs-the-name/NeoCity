1f229e8e5dd5fa70e5d0920b5838260acdcc0ab4
Ehh-the-name <keffendi2010@gmail.com>
2026-07-28 19:31:52 -0400

    js for font change

--- mp-script.js
+++ mp-script.js
@@ -12,3 +12,19 @@ function changeSVG(event){
         img.src = "https://i.postimg.cc/440ppw5r/Webbed-Site.png";
     }   
 }
+
+function fontChange(){
+    const eleList = document.querySelectorAll('[class="fonter"]');
+   
+    for(let i=0; i < eleList.length; i++) {
+        console.log(eleList[i].nodeName);
+
+        if(eleList[i].style.fontFamily == 'OpenDyslexic') {
+            eleList[i].style.fontFamily = 'MatrixType';
+            eleList[i].style.fontSize = '1rem';
+        } else {
+            eleList[i].style.fontFamily = 'OpenDyslexic';
+            eleList[i].style.fontSize = '0.75rem';
+        }
+    }
+}
